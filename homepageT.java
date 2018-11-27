172c25b2bbaf179ef04bfa6e032ca1f804e7d69d
DanRyder98 <45339143+DanRyder98@users.noreply.github.com>
2018-11-27 16:17:27 +0000

    Design changes

--- homepageT.java
+++ homepageT.java
@@ -1,4 +1,4 @@
-//home page for Registrar
+//home page for Teacher
 
 import java.awt.*;
 import javax.swing.*;
@@ -16,33 +16,165 @@ public class homepageT extends JFrame{
 		 setDefaultCloseOperation(EXIT_ON_CLOSE);
 		 
 		 
-		 //create the container and setlayout
-		 Container contentPane = getContentPane();
-		 contentPane.setLayout(new GridLayout());
-		 
-		 // create label
-		 //JLabel label = new JLabel ("Menu: ");
-		 //contentPane.add(label, BorderLayout.NORTH);
-		 //label.setPreferredSize(new Dimension(50, 20));
-		 
-		 // create buttons 
-		 JButton addGrades = new JButton("Add Grades");
-		 JButton updateGrades = new JButton("Update Grades");
-		 JButton meanGrade = new JButton("Mean Grade");
-		
-		
-		 
-		 //adding buttons
-		 contentPane.setLayout(new GridLayout(1,3));
-		 contentPane.add(addGrades);
-		 contentPane.add(updateGrades);
-		 contentPane.add(meanGrade);
+		initComponents();
 		 
 		 
 		 setVisible(true);
 	 }
-	 public static void main(String[] args) {
-		 new homepageT("Teacher menu");
+         
+        private void initComponents() {
+
+            jLabel2 = new javax.swing.JLabel();
+            jButton1 = new javax.swing.JButton();
+            jButton2 = new javax.swing.JButton();
+            jButton3 = new javax.swing.JButton();
+            jButton4 = new javax.swing.JButton();
+            jLabel1 = new javax.swing.JLabel();
+            jButton5 = new javax.swing.JButton();
+            jButton6 = new javax.swing.JButton();
+
+            setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+            setPreferredSize(new java.awt.Dimension(1920, 1080));
+
+            jLabel2.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+            jLabel2.setText("Teacher");
+
+            jButton1.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+            jButton1.setText("Add Grades");
+            jButton1.setToolTipText("Add initial/ressit/repeat grades");
+            jButton1.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton1ActionPerformed(evt);
+                }
+            });
+
+            jButton2.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+            jButton2.setText("Update Grades");
+            jButton2.setToolTipText("Update initial/resit/repeat grades");
+            jButton2.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton2ActionPerformed(evt);
+                }
+            });
+
+            jButton3.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+            jButton3.setText("Mean Grades");
+            jButton3.setToolTipText("Calculate the weighted mean grade for a period to check if student has passed");
+            jButton3.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton3ActionPerformed(evt);
+                }
+            });
+
+            jButton4.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+            jButton4.setText("Degree Result");
+            jButton4.setToolTipText("Calculate overall degree results to determine degree class");
+            jButton4.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton4ActionPerformed(evt);
+                }
+            });
+
+            jLabel1.setFont(new java.awt.Font("Verdana", 0, 24)); // NOI18N
+            jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+            jLabel1.setText("Welcome");
+
+            jButton5.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+            jButton5.setText("Log Out");
+            jButton5.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton5ActionPerformed(evt);
+                }
+            });
+
+            jButton6.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+            jButton6.setText("Student Status");
+            jButton6.setToolTipText("View the current status of any student showing their grades");
+            jButton6.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    jButton6ActionPerformed(evt);
+                }
+            });
+
+            javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+            getContentPane().setLayout(layout);
+            layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1004, Short.MAX_VALUE)
+                        .addComponent(jButton4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                            .addGap(0, 0, Short.MAX_VALUE)
+                            .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jButton6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addContainerGap())
+            );
+            layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jLabel2)
+                    .addGap(46, 46, 46)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGap(160, 160, 160)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGap(18, 18, 18)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGap(18, 18, 18)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGap(18, 18, 18)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGap(18, 18, 18)
+                    .addComponent(jButton6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGap(66, 66, 66)
+                    .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addContainerGap())
+            );
+
+            pack();
+        }
+         
+        private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+            
+        }                                        
+
+        private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+            
+        }                                        
+
+        private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+            
+        }                                        
+
+        private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+            
+        }                                        
+
+        private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+            
+        }                                        
+
+        private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+            
+        } 
+         
+	public static void main(String[] args) {
+		new homepageT("Teacher menu");
 		 
-	 }
+	}
+         
+        private javax.swing.JButton jButton1;
+        private javax.swing.JButton jButton2;
+        private javax.swing.JButton jButton3;
+        private javax.swing.JButton jButton4;
+        private javax.swing.JButton jButton5;
+        private javax.swing.JButton jButton6;
+        private javax.swing.JLabel jLabel1;
+        private javax.swing.JLabel jLabel2;
 }
