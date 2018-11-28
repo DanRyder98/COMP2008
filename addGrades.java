6d4a5f3b09d9364afb6c91d56379ce3a90c6e3ee
marwanhegazy <mmhegazy3@sheffield.ac.uk>
2018-11-28 15:55:14 +0000

    added back button

--- addGrades.java
+++ addGrades.java
@@ -62,18 +62,29 @@ public class addGrades extends JFrame {
 	 JButton button = new JButton("add grade");
 	 contentPane.add(button, BorderLayout.CENTER);
 	 
+	 JButton backbutton = new JButton("Back");
+	 contentPane.add(backbutton, BorderLayout.CENTER);
 	 
 	 button.addActionListener(new java.awt.event.ActionListener() {
          public void actionPerformed(java.awt.event.ActionEvent evt) {
              buttonActionPerformed(evt);
              }
          });
+	  backbutton.addActionListener(new java.awt.event.ActionListener() {
+         public void actionPerformed(java.awt.event.ActionEvent evt) {
+             BackButtonActionPerformed(evt);
+             }
+	  });
 	 setVisible(true);
 	 
 	 }
 	 private void buttonActionPerformed(java.awt.event.ActionEvent evt) {                                         
          
      }
+	 private void BackButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
+	        this.setVisible(false);
+	        new homepageT("homepage").setVisible(true);
+	    }        
 	 public static void main(String[] args) {
 		 new addGrades("add grades for a student");
 	 }
