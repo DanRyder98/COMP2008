d6811633578be9322812ee43aa522d9fac13bafe
DanRyder98 <45339143+DanRyder98@users.noreply.github.com>
2018-12-04 01:38:03 +0000

    Update HomePageAdministrator.java

--- HomePageAdministrator.java
+++ HomePageAdministrator.java
@@ -1,4 +1,7 @@
 package Admin;
+import java.awt.*;
+import javax.swing.*;
+import java.sql.*;
 
 /**
  *
@@ -14,19 +17,17 @@ public class HomePageAdministrator extends javax.swing.JFrame {
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         ManageAccountsButton = new javax.swing.JButton();
-        AddDepartmentsButton = new javax.swing.JButton();
+        ManageDepartmentsButton = new javax.swing.JButton();
         WelcomeLabel = new javax.swing.JLabel();
         AdminLabel = new javax.swing.JLabel();
         LogOutButton = new javax.swing.JButton();
-        RemoveDepartmentsButton = new javax.swing.JButton();
-        AddDegreeCoursesButton = new javax.swing.JButton();
-        RemoveDegreeCoursesButton = new javax.swing.JButton();
-        AddModulesOverallButton = new javax.swing.JButton();
-        RemoveModulesOverallButton = new javax.swing.JButton();
+        ManageDegreeButton = new javax.swing.JButton();
+        ManageModulesButton = new javax.swing.JButton();
+        RemoveDepartmentDegreeAssociationButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -39,12 +40,12 @@ public class HomePageAdministrator extends javax.swing.JFrame {
             }
         });
 
-        AddDepartmentsButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
-        AddDepartmentsButton.setText("Add Departments");
-        AddDepartmentsButton.setToolTipText("Update initial/resit/repeat grades");
-        AddDepartmentsButton.addActionListener(new java.awt.event.ActionListener() {
+        ManageDepartmentsButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        ManageDepartmentsButton.setText("Manage Departments");
+        ManageDepartmentsButton.setToolTipText("Update initial/resit/repeat grades");
+        ManageDepartmentsButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                AddDepartmentsButtonActionPerformed(evt);
+                ManageDepartmentsButtonActionPerformed(evt);
             }
         });
 
@@ -63,48 +64,30 @@ public class HomePageAdministrator extends javax.swing.JFrame {
             }
         });
 
-        RemoveDepartmentsButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
-        RemoveDepartmentsButton.setText("Remove Departments");
-        RemoveDepartmentsButton.setToolTipText("Add initial/ressit/repeat grades");
-        RemoveDepartmentsButton.addActionListener(new java.awt.event.ActionListener() {
+        ManageDegreeButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        ManageDegreeButton.setText("Manage Degree");
+        ManageDegreeButton.setToolTipText("Add initial/ressit/repeat grades");
+        ManageDegreeButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                RemoveDepartmentsButtonActionPerformed(evt);
+                ManageDegreeButtonActionPerformed(evt);
             }
         });
 
-        AddDegreeCoursesButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
-        AddDegreeCoursesButton.setText("Add Degree Courses");
-        AddDegreeCoursesButton.setToolTipText("Add initial/ressit/repeat grades");
-        AddDegreeCoursesButton.addActionListener(new java.awt.event.ActionListener() {
+        ManageModulesButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        ManageModulesButton.setText("Manage Modules");
+        ManageModulesButton.setToolTipText("Add initial/ressit/repeat grades");
+        ManageModulesButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                AddDegreeCoursesButtonActionPerformed(evt);
+                ManageModulesButtonActionPerformed(evt);
             }
         });
 
-        RemoveDegreeCoursesButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
-        RemoveDegreeCoursesButton.setText("Remove Degree Courses");
-        RemoveDegreeCoursesButton.setToolTipText("Add initial/ressit/repeat grades");
-        RemoveDegreeCoursesButton.addActionListener(new java.awt.event.ActionListener() {
+        RemoveDepartmentDegreeAssociationButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        RemoveDepartmentDegreeAssociationButton.setText("Remove Department Degree Association");
+        RemoveDepartmentDegreeAssociationButton.setToolTipText("Add initial/ressit/repeat grades");
+        RemoveDepartmentDegreeAssociationButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                RemoveDegreeCoursesButtonActionPerformed(evt);
-            }
-        });
-
-        AddModulesOverallButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
-        AddModulesOverallButton.setText("Add Modules Overall");
-        AddModulesOverallButton.setToolTipText("Add initial/ressit/repeat grades");
-        AddModulesOverallButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                AddModulesOverallButtonActionPerformed(evt);
-            }
-        });
-
-        RemoveModulesOverallButton.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
-        RemoveModulesOverallButton.setText("Remove Modules Overall");
-        RemoveModulesOverallButton.setToolTipText("Add initial/ressit/repeat grades");
-        RemoveModulesOverallButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                RemoveModulesOverallButtonActionPerformed(evt);
+                RemoveDepartmentDegreeAssociationButtonActionPerformed(evt);
             }
         });
 
@@ -118,15 +101,13 @@ public class HomePageAdministrator extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(LogOutButton, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(RemoveDepartmentsButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(ManageAccountsButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(AddDepartmentsButton, javax.swing.GroupLayout.DEFAULT_SIZE, 1060, Short.MAX_VALUE)
-                    .addComponent(AddDegreeCoursesButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(RemoveDegreeCoursesButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(AddModulesOverallButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(RemoveModulesOverallButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(ManageDepartmentsButton, javax.swing.GroupLayout.DEFAULT_SIZE, 1060, Short.MAX_VALUE)
+                    .addComponent(ManageDegreeButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(ManageModulesButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(WelcomeLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(AdminLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(AdminLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(RemoveDepartmentDegreeAssociationButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -136,67 +117,53 @@ public class HomePageAdministrator extends javax.swing.JFrame {
                 .addComponent(AdminLabel)
                 .addGap(50, 50, 50)
                 .addComponent(WelcomeLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(66, 66, 66)
+                .addGap(88, 88, 88)
                 .addComponent(ManageAccountsButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
-                .addComponent(AddDepartmentsButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(ManageDepartmentsButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
-                .addComponent(RemoveDepartmentsButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(ManageDegreeButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
-                .addComponent(AddDegreeCoursesButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(RemoveDepartmentDegreeAssociationButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
-                .addComponent(RemoveDegreeCoursesButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
-                .addComponent(AddModulesOverallButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
-                .addComponent(RemoveModulesOverallButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(58, 58, 58)
+                .addComponent(ManageModulesButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(138, 138, 138)
                 .addComponent(LogOutButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void ManageAccountsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ManageAccountsButtonActionPerformed
+    private void ManageAccountsButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                     
         this.setVisible(false);
         new ManageAccounts().setVisible(true);
-    }//GEN-LAST:event_ManageAccountsButtonActionPerformed
+    }                                                    
 
-    private void AddDepartmentsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddDepartmentsButtonActionPerformed
+    private void ManageDepartmentsButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                        
         this.setVisible(false);
-        new AddDepartments().setVisible(true);
-    }//GEN-LAST:event_AddDepartmentsButtonActionPerformed
+        new ManageDepartments().setVisible(true);
+    }                                                       
 
-    private void LogOutButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LogOutButtonActionPerformed
+    private void LogOutButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
         this.setVisible(false);
         new LoggingIn().setVisible(true);
-    }//GEN-LAST:event_LogOutButtonActionPerformed
+    }                                            
 
-    private void RemoveDepartmentsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RemoveDepartmentsButtonActionPerformed
+    private void ManageDegreeButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                   
         this.setVisible(false);
-        new RemoveDepartments().setVisible(true);
-    }//GEN-LAST:event_RemoveDepartmentsButtonActionPerformed
+        new ManageDegree().setVisible(true);
+    }                                                  
 
-    private void AddDegreeCoursesButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddDegreeCoursesButtonActionPerformed
+    private void ManageModulesButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                    
         this.setVisible(false);
-        new AddDegreeCourse().setVisible(true);
-    }//GEN-LAST:event_AddDegreeCoursesButtonActionPerformed
+        new ManageModules().setVisible(true);
+    }                                                   
 
-    private void RemoveDegreeCoursesButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RemoveDegreeCoursesButtonActionPerformed
+    private void RemoveDepartmentDegreeAssociationButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                                        
         this.setVisible(false);
-        new RemoveDegreeCourse().setVisible(true);
-    }//GEN-LAST:event_RemoveDegreeCoursesButtonActionPerformed
-
-    private void AddModulesOverallButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddModulesOverallButtonActionPerformed
-        this.setVisible(false);
-        new AddModulesOverall().setVisible(true);
-    }//GEN-LAST:event_AddModulesOverallButtonActionPerformed
-
-    private void RemoveModulesOverallButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RemoveModulesOverallButtonActionPerformed
-        this.setVisible(false);
-        new RemoveModulesOverall().setVisible(true);
-    }//GEN-LAST:event_RemoveModulesOverallButtonActionPerformed
+        new RemoveDepartmentDegreeAssociation().setVisible(true);
+    }                                                                       
 
     public static void main(String args[]) {
         try {
@@ -225,16 +192,14 @@ public class HomePageAdministrator extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton AddDegreeCoursesButton;
-    private javax.swing.JButton AddDepartmentsButton;
-    private javax.swing.JButton AddModulesOverallButton;
+    // Variables declaration - do not modify                     
     private javax.swing.JLabel AdminLabel;
     private javax.swing.JButton LogOutButton;
     private javax.swing.JButton ManageAccountsButton;
-    private javax.swing.JButton RemoveDegreeCoursesButton;
-    private javax.swing.JButton RemoveDepartmentsButton;
-    private javax.swing.JButton RemoveModulesOverallButton;
+    private javax.swing.JButton ManageDegreeButton;
+    private javax.swing.JButton ManageDepartmentsButton;
+    private javax.swing.JButton ManageModulesButton;
+    private javax.swing.JButton RemoveDepartmentDegreeAssociationButton;
     private javax.swing.JLabel WelcomeLabel;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
